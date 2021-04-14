22481a8b98affa16b744d147b9e480c4f7e48df4
Kanomble <lujebecker@googlemail.com>
2021-04-14 23:07:09 +0200

    added view, base template and url for create_taxonomic_files, refactored urls.py in blast_project app

--- celery_blast/blast_project/urls.py
+++ celery_blast/blast_project/urls.py
@@ -2,13 +2,27 @@
 urls for blast projects
 '''
 
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-urlpatterns = [
-    path('', views.dashboard, name='blast_project_dashboard'),
+registration_urls = [
     path('login', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
-    path('register', views.registration_view, name='register'),
-    path('failure', views.failure_view, name='failure_view'),
+    path('register', views.registration_view, name='register')
+]
+
+service_urls = [
+    path('create_taxonomic_file', views.create_taxonomic_file_view, name='species_taxids')
+]
+
+
+success_failure_urls = [
+    path('failure', views.failure_view, name='failure_view')
+]
+urlpatterns = [
+    path('', views.dashboard_view, name='blast_project_dashboard'),
+
+    path('', include(registration_urls)),
+    path('', include(service_urls)),
+    path('', include(success_failure_urls))
 ]
--- celery_blast/blast_project/views.py
+++ celery_blast/blast_project/views.py
@@ -5,28 +5,40 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from .forms import CreateUserForm
 
+''' dashboard
+
+view for the first dashboard page, this page enables monitoring of blast_projects,
+created by the currently logged in user.
+
+:GET
+    display blast_projects and links to other view functions
+
+'''
 @login_required(login_url='login')
-def dashboard(request):
-    ''' NOT NEEDED ANYMORE
-    try:
-        projects = BlastProject.objects.filter(project_username=request.user)
-        #query set of project specific databases
-        target_genomes = Genomes.objects.filter(associated_project__in=projects)
-        #deletes folders that are not included as projects ids in the database
-        #delete_files_without_projects()
-    except Exception as e:
-        return failure_view(request,e)
-    context = {
-        'projects':projects,
-        'genomes':target_genomes,
-        }
-    '''
+def dashboard_view(request):
     context={}
     return render(request,'blast_project/blast_project_dashboard.html',context)
 
-#register an account with email and password, email can be used inside biopython functions
-#user needs to authenticate otherwise they will get redirected to this login page
+''' create_taxonomic_file_view
 
+view for creation of taxonomic files, produced by the get_species_taxids.sh script.
+
+:GET
+    display available taxonomic files
+:POST
+    create taxonomic files
+
+'''
+@login_required(login_url='login')
+def create_taxonomic_file_view(request):
+    context={}
+    return render(request,'blast_project/create_taxonomic_file.html',context)
+
+''' registration, login and logout views
+register an account with email and password, email can be used inside biopython functions
+user needs to authenticate otherwise they will get redirected to this login page
+
+'''
 #login user
 @unauthenticated_user #you dont need an account to trigger this view
 def login_user(request):
@@ -68,6 +80,13 @@ def registration_view(request):
     context = {'form': userForm, }
     return render(request,'blast_project/register.html',context)
 
+''' failure view
+
+returned if an exception ocurred within execution of view functions.
+
+:param exception
+    :type str
+'''
 #if an exception occurres this page is rendered in order to evaluate the exception context
 def failure_view(request,exception):
     context={'exception':exception}
