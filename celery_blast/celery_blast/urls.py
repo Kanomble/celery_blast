82a902bcbbbf0b29739a81e00840675e92164484
Kanomble <lujebecker@googlemail.com>
2021-04-16 11:54:03 +0200

    added static css script, styled database and project divs, headers, added background image, fixed favicon

--- celery_blast/celery_blast/urls.py
+++ celery_blast/celery_blast/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^favicon\.ico$', favicon_view),
     path('blast_project/', include('blast_project.urls')),
     path('refseq_transactions/',include('refseq_transactions.urls')),
 ]
