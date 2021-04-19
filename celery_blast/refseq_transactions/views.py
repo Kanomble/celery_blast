71158ea4a32598fefdfe0a19fd67964800f87b51
Kanomble <lujebecker@googlemail.com>
2021-04-19 11:53:56 +0200

    Update documentation on view function for refseq transactions

--- celery_blast/refseq_transactions/views.py
+++ celery_blast/refseq_transactions/views.py
@@ -8,6 +8,14 @@ from .forms import RefseqTableForm
 from .tasks import download_refseq_assembly_summary_file
 from blast_project.views import failure_view
 
+''' dashboard
+    
+    dashboard for refseq databases, lists all available BLAST databases, 
+    download button for the refseq assembly summary file
+    
+    url: refseq_transactions_dashboard
+
+'''
 @login_required(login_url='login')
 def dashboard(request):
     try:
@@ -24,6 +32,15 @@ def dashboard(request):
     except Exception as e:
         return failure_view(request,e)
 
+''' download_refseq_assembly_summary_view
+    
+    triggers a @shared_task function in order to download the current refseq summary file
+    from the ncbi ftp directory
+    redirects to the refseq_transactions_dashboard url e.g. the dashboard view above
+    
+    url: download_refseq_assembly_summary
+    
+'''
 @login_required(login_url='login')
 def download_refseq_assembly_summary_view(request):
     try:
