b64dae5d8117a87285950ce109b365d50670082a
Kanomble <lujebecker@googlemail.com>
2021-08-27 12:19:13 +0200

    added routing specifications for test purposes

--- flask_api_bioinformatic_tools/flask_api_bioinformatic_tools/fmafft.py
+++ flask_api_bioinformatic_tools/flask_api_bioinformatic_tools/fmafft.py
@@ -9,5 +9,9 @@ fmafft = Blueprint('fmafft',__name__)
 
 @fmafft.route('/mafft_info',methods=['GET'])
 def index():
-    print("[+] Connectied to flask_api_bioinformatic_tools container, welcome!")
-    return "Hello World!"
+    #print("[+] Connected to flask_api_bioinformatic_tools container, welcome!")
+    return "Connected to flask_api_bioinformatic_tools container, welcome!"
+
+@fmafft.route('/project_information/<int:project_id>',methods=['GET'])
+def list_informations(project_id):
+    return "listing view of project : {}".format(project_id)
