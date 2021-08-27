70d5b257f4857e5bd93c410fec03cb7291d09377
Kanomble <lujebecker@googlemail.com>
2021-08-27 13:57:50 +0200

    added table for the external tool dashboard with form submit to the flask api container

--- celery_blast/external_tools/views.py
+++ celery_blast/external_tools/views.py
@@ -11,6 +11,7 @@ def project_informations(request, project_id):
         context = {}
         qseqid_folder = get_list_of_query_sequence_folder(project_id)
         context['qseqid_folder'] = qseqid_folder
+        context['project_id'] = project_id
         return render(request,"external_tools/external_tools_dashboard.html",context)
     except Exception as e:
         return failure_view(request,e)
