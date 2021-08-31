f3fb6e58ab373ea8a4b14c1e258d2846ad3f3084
Lukas Becker <lujebecker@googlemail.com>
2021-08-31 11:21:53 +0200

    refactored snakefile and workflow

--- celery_blast/static/snakefiles/reciprocal_blast/build_folders_with_hit_info_for_each_qseqid.py
+++ celery_blast/static/snakefiles/reciprocal_blast/build_folders_with_hit_info_for_each_qseqid.py
@@ -3,14 +3,14 @@ import os
 
 result_df = pd.read_csv(snakemake.input['result_csv'],header=0, index_col=0)
 hit_info_file = open(snakemake.output['hit_information'],'w')
-for query in snakemake.output['queries']:
+for query in result_df['qseqid'].unique():
 
     target_df = result_df[result_df['qseqid'] == query]
     sacc_list = list(target_df['sacc'].unique())
 
     output_file_path = query + '/' + 'target_sequence_ids.txt'
 
-    output = open(query,'w')
+    output = open(output_file_path,'w')
     for sacc in sacc_list:
         output.write(sacc+'\n')
     output.close()
