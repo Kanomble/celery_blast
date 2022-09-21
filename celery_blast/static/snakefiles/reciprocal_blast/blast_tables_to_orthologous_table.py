5e7803d632cfea0392d6b7a86641f6ba67fcff7a
kanomble <lujebecker@googlemail.com>
2022-09-21 18:55:34 +0200

    changed time limits in settings.py refactored add_taxonomic_informatio_to_result_dataframe function in blast_tables_to_orthologous_table.py script (part of the snakemake execution) added chmod u+x wait-for in docker-compose.yml refactored ete3_tree_to_png.py if strange organism names appear

--- celery_blast/static/snakefiles/reciprocal_blast/blast_tables_to_orthologous_table.py
+++ celery_blast/static/snakefiles/reciprocal_blast/blast_tables_to_orthologous_table.py
@@ -5,13 +5,14 @@ import sys
 from Bio import Entrez
 
 ERRORCODE=6
-with open(snakemake.log['log'],'w') as logfile:
+with open(snakemake.log['log'], 'w') as logfile:
     try:
         logfile.write("INFO:starting to construct result dataframe ...\n")
         Entrez.email = snakemake.config['user_email']
-        rec_prot=pd.read_table(snakemake.input['rec_res'],index_col=False)
-        fw_res=pd.read_table(snakemake.input['fw_res'],header=None)
-        fw_res.columns=["qseqid", "sseqid", "pident", "evalue", "bitscore", "qgi", "sgi", "sacc", "staxids", "sscinames", "scomnames",
+        rec_prot = pd.read_table(snakemake.input['rec_res'], index_col=False)
+        fw_res = pd.read_table(snakemake.input['fw_res'], header=None)
+        fw_res.columns = ["qseqid", "sseqid", "pident", "evalue", "bitscore", "qgi", "sgi", "sacc", "staxids",
+                          "sscinames", "scomnames",
                           "stitle"]
 
         fw_res['qseqid'] = fw_res['qseqid'].map(lambda line: line.split('.')[0])
@@ -26,10 +27,11 @@ with open(snakemake.log['log'],'w') as logfile:
         result_data = result_data.drop_duplicates(['sacc_transformed', 'staxids'], keep='first')
         result_data = result_data.reset_index(drop=True)
 
+
         # Adds taxonomic information to pandas dataframe.
         # In order to retrieve the taxonomic information biopython calls are conducted with the query accession ids in the pandas dataframe.
         # The query file gets processed for extracting the fasta header line, this information is then loaded into a specific dataframe column.
-        def add_taxonomic_information_to_result_dataframe(result_data, query_file):
+        def add_taxonomic_information_to_result_dataframe(taxids, query_file):
             def read_query_file(query_file):
                 queries = {}
                 queryfile = open(query_file, "r")
@@ -41,123 +43,129 @@ with open(snakemake.log['log'],'w') as logfile:
                 queryfile.close()
                 return queries
 
-
-            df = result_data.copy()
             queries = read_query_file(query_file)
-            dataframes = []
-            unique_queries = list(df['qseqid'].unique())
 
-            logfile.write("INFO:working on {} unique queries\n".format(len(unique_queries)))
-            for query in unique_queries:
-                dataframe = df.loc[df['qseqid'] == query].copy()
-                dataframe['sacc'] = dataframe['sacc'].map(lambda protid: protid.split(".")[0])
-                dataframe = dataframe.drop_duplicates(subset=['sacc'], keep="first")
-                #if len(dataframe) >= 4999:
-                #    dataframe = dataframe[0:4999]
-                staxids = []
-                for ids in list(dataframe['staxids']):
-                    if type(ids) == str:
-                        staxids.append(ids.split(";")[0])
-                    elif type(ids) == int:
-                        staxids.append(ids)
+            logfile.write("INFO:working on {} taxids\n".format(len(taxids)))
 
-                result_record = []
+            query_info = []
+            taxid = []
+            taxonomy = []
+            genus = []
+            superfamily = []
+            family = []
+            order = []
+            classt = []
+            phylum = []
 
-                end = len(dataframe[dataframe['qseqid'] == query])
-                begin = 0
-                step = 500
-                steps = 500
-                logfile.write("INFO:inference of taxonomic informations for all rbhs of {}\n".format(query,len(dataframe)))
-                while begin < end:
-                    if step >= end:
-                        step = end
-                    splitted_ids = staxids[begin:step]
-                    # range(X) tries for biopython calls
-                    for attempt in range(10):
-                        try:
-                            handle = Entrez.efetch(id=splitted_ids, db="taxonomy", retmode="xml")
-                            record = Entrez.read(handle)
-                            handle.close()
-                        except Exception as e:
-                            if attempt == 9:
-                                logfile.write("ERROR:inference of taxonomic informations failed for query sequence dataframe of {} with exception {}\n".format(query,e))
-                                sys.exit(ERRORCODE)
-                        else:
-                            for rec in record:
-                                result_record.append(rec)
-                            break
-                    begin += steps
-                    step += steps
+            end = len(taxids)
+            begin = 0
+            step = 500
+            steps = 500
+            logfile.write("INFO:inference of taxonomic informations for all {} taxids\n".format(len(taxids)))
+            while begin < end:
+                if step >= end:
+                    step = end
+                splitted_ids = taxids[begin:step]
+                # range(X) tries for biopython calls
+                for attempt in range(10):
+                    try:
+                        handle = Entrez.efetch(id=splitted_ids, db="taxonomy", retmode="xml")
+                        record = Entrez.read(handle)
+                        handle.close()
+                    except Exception as e:
+                        if attempt == 9:
+                            logfile.write(
+                                "ERROR:inference of taxonomic informations failed with exception {}\n".format(e))
+                            sys.exit(ERRORCODE)
+                    else:
+                        for i in range(len(record)):
+                            taxonomy.append(record[i]['ScientificName'])
+                            if 'AkaTaxIds' in record[i].keys():
+                                for akaid in record[i]['AkaTaxIds']:
+                                    if int(akaid) in splitted_ids:
+                                        taxid.append(akaid)
+                                        logfile.write("\tINFO: AkaTaxIds detected: {}\n".format(akaid))
+                                        break
+                                else:
+                                    taxid.append(record[i]['TaxId'])
+                            else:
+                                taxid.append(record[i]['TaxId'])
+                            for j in record[i]['LineageEx']:
+                                if j['Rank'] == 'genus':
+                                    genus.append(j['ScientificName'])
+                                if j['Rank'] == 'superfamily':
+                                    superfamily.append(j['ScientificName'])
+                                if j['Rank'] == 'family':
+                                    family.append(j['ScientificName'])
+                                if j['Rank'] == 'order':
+                                    order.append(j['ScientificName'])
+                                if j['Rank'] == 'class':
+                                    classt.append(j['ScientificName'])
+                                if j['Rank'] == 'phylum':
+                                    phylum.append(j['ScientificName'])
 
-                query_info = []
-                taxonomy = []
-                genus = []
-                superfamily = []
-                family = []
-                order = []
-                classt = []
-                phylum = []
-                for i in range(len(result_record)):
-                    query_info.append(queries[query])
-                    taxonomy.append(result_record[i]['ScientificName'])
-                    for j in result_record[i]['LineageEx']:
-                        if j['Rank'] == 'genus':
-                            genus.append(j['ScientificName'])
-                        if j['Rank'] == 'superfamily':
-                            superfamily.append(j['ScientificName'])
-                        if j['Rank'] == 'family':
-                            family.append(j['ScientificName'])
-                        if j['Rank'] == 'order':
-                            order.append(j['ScientificName'])
-                        if j['Rank'] == 'class':
-                            classt.append(j['ScientificName'])
-                        if j['Rank'] == 'phylum':
-                            phylum.append(j['ScientificName'])
+                            if (len(taxonomy) != len(genus)):
+                                genus.append('unknown')
+                            if (len(taxonomy) != len(superfamily)):
+                                superfamily.append('unknown')
+                            if (len(taxonomy) != len(family)):
+                                family.append('unknown')
+                            if (len(taxonomy) != len(order)):
+                                order.append('unknown')
+                            if (len(taxonomy) != len(classt)):
+                                classt.append('unknown')
+                            if (len(taxonomy) != len(phylum)):
+                                phylum.append('unknown')
 
-                    if (len(taxonomy) != len(genus)):
-                        genus.append('unknown')
-                    if (len(taxonomy) != len(superfamily)):
-                        superfamily.append('unknown')
-                    if (len(taxonomy) != len(family)):
-                        family.append('unknown')
-                    if (len(taxonomy) != len(order)):
-                        order.append('unknown')
-                    if (len(taxonomy) != len(phylum)):
-                        phylum.append('unknown')
-                    if (len(taxonomy) != len(classt)):
-                        classt.append('unknown')
+                            if len(record) != len(splitted_ids):
+                                missing_ids = [m_taxid for m_taxid in splitted_ids if m_taxid not in taxid]
+                                for m_taxid in missing_ids:
+                                    logfile.write(
+                                        "WARNING: problem during fetching of taxonomic information for: {}\n".format(
+                                            m_taxid))
+                                    taxid.append(m_taxid)
+                                    taxonomy.append('unknown')
+                                    genus.append('unknown')
+                                    superfamily.append('unknown')
+                                    family.append('unknown')
+                                    order.append('unknown')
+                                    classt.append('unknown')
+                                    phylum.append('unknown')
+                        break
+                logfile.write("INFO: Done with chunk: {} - {}\n".format(begin, step))
 
-                # compare length of all informations - they should have the same length
-                if (len(genus) == len(dataframe) and len(family) == len(dataframe) and len(superfamily) == len(
-                        dataframe) and len(
-                        query_info) == len(dataframe)):
-                    dataframe['genus'] = genus
-                    dataframe['superfamily'] = superfamily
-                    dataframe['family'] = family
-                    dataframe['order'] = order
-                    dataframe['phylum'] = phylum
-                    dataframe['class'] = classt
-                    dataframe['query_info'] = query_info
-                else:
-                    logfile.write(
-                        "ERROR:Informations from biopython calls have not the same length as the dataframe,\
-                                    dataframe cant get combined with taxonomic information'\n")
-                    sys.exit(ERRORCODE)
+                begin += steps
+                step += steps
 
-                dataframes.append(dataframe)
-                logfile.write("INFO:extracting taxonomic informations for query sequence {}\n".format(query))
+            # compare length of all informations - they should have the same length
+            if (len(genus) == len(taxids) and len(family) == len(taxids) and len(superfamily) == len(taxids)):
+                columns = ["staxids", 'organism_name_taxdb', 'genus', 'family', 'superfamily', 'order', 'class',
+                           'phylum']
+                tax_db = pd.DataFrame([taxid, taxonomy, genus, family, superfamily, order, classt, phylum])
+                tax_db = tax_db.transpose()
+                tax_db.columns = columns
+                return tax_db
+            else:
+                logfile.write(
+                    "ERROR:Informations from biopython calls have not the same length as the dataframe,\
+                                dataframe cant get combined with taxonomic information'\n")
+                sys.exit(ERRORCODE)
 
-            result_df = pd.concat(dataframes)
 
-            return result_df
+        taxids_from_df = result_data['staxids'].unique()
+        logfile.write("INFO:constructing result dataframe\n")
+        result_data.to_csv(snakemake.output['result_csv'], header=list(result_data.columns))
+        taxonomy_dataframe = add_taxonomic_information_to_result_dataframe(taxids_from_df, snakemake.input['query_file'])
+        taxonomy_dataframe['staxids'] = taxonomy_dataframe['staxids'].astype('int64')
+        result_data = result_data.merge(taxonomy_dataframe, on='staxids')
+        result_data['query_info'] = result_data['stitle']
+
+        logfile.write("INFO:writing result dataframe\n")
+        logfile.write("DONE\n")
         logfile.write("INFO:constructing result dataframe\n")
 
-        result_data.to_csv(snakemake.output['result_csv'],header=list(result_data.columns))
-
-        big_result_data = add_taxonomic_information_to_result_dataframe(result_data, snakemake.input['query_file'])
         logfile.write("INFO:writing result dataframe\n")
-
-        big_result_data.to_csv(snakemake.output['taxonomy_result_csv'],header=list(big_result_data.columns))
+        result_data.to_csv(snakemake.output['taxonomy_result_csv'], header=list(result_data.columns))
         logfile.write("DONE\n")
 
     except Exception as e:
--- celery_blast/static/snakefiles/reciprocal_blast/ete3_tree_to_png.py
+++ celery_blast/static/snakefiles/reciprocal_blast/ete3_tree_to_png.py
@@ -20,7 +20,7 @@ with open(snakemake.log['log'],'w') as logfile:
             shutil.copyfile('../../../static/images/no_results.svg', snakemake.params['static_pic'])
             shutil.copyfile('../../../static/images/no_results.svg', snakemake.output['pic'])
         else:
-            tree = tree[0]
+            tree = tree[0].replace("'","")
             logfile.write("INFO:converting tree to ete3 object\n")
             tree = ete3.Tree(tree)
             ts = ete3.TreeStyle()
@@ -31,5 +31,24 @@ with open(snakemake.log['log'],'w') as logfile:
             tree.render(snakemake.output['pic'], tree_style=ts)
         logfile.write("DONE\n")
     except Exception as e:
-        logfile.write("ERROR:treefile couldn't get converted to png image - {}\n".format(e))
-        exit(ERRORCODE)
+        try:
+            with open(snakemake.input['nw'], 'r') as t:
+                tree = t.readlines()
+            if len(tree) < 1:
+                with open(snakemake.log['log'], 'w') as log_f:
+                    log_f.write('ERROR: The Newick file used in this command has no content.')
+                shutil.copyfile('../../../static/images/no_results.svg', snakemake.params['static_pic'])
+                shutil.copyfile('../../../static/images/no_results.svg', snakemake.output['pic'])
+            else:
+                tree = tree[0].replace("'", "")
+                logfile.write("INFO:using normal treefile\n")
+                tree = ete3.Tree(tree)
+                ts = ete3.TreeStyle()
+                ts.show_branch_length = True
+                ts.title.add_face(ete3.TextFace('Phylogenetic tree of ' + str(query), fsize=20), column=0)
+                logfile.write("INFO:generating png image\n")
+                tree.render(snakemake.params['static_pic'], tree_style=ts)
+                tree.render(snakemake.output['pic'], tree_style=ts)
+        except Exception as e:
+            logfile.write("ERROR:treefile couldn't get converted to png image - {}\n".format(e))
+            exit(ERRORCODE)
