70b30228fdb75aac8f0c274111d371a9c0ac7ff9
Kanomble <lujebecker@googlemail.com>
2022-09-22 17:39:56 +0200

    refactored calculate_database_statistics function in py_database_statistics.py

--- celery_blast/blast_project/py_database_statistics.py
+++ celery_blast/blast_project/py_database_statistics.py
@@ -5,6 +5,7 @@ from os.path import isfile, isdir
 from os import remove, listdir
 from django.db import IntegrityError
 from Bio import Entrez
+import json
 
 '''add_taxonomic_information_to_db
 
@@ -124,8 +125,151 @@ def add_taxonomic_information_to_db(user_email: str,logfile:str, taxids:list) ->
         raise Exception("ERROR during database table extension with taxonomic information with excpetion: {}".format(e))
 
 
+'''extract_taxonomic_information
+
+    This function loops over the queries in the reciprocal result dataframe. 
+    Each query specific result dataframe is getting merged with the dataframe of the database that inherits the taxonomic informations.
+    Therefore it
+
+    :param logfile
+        :type str
+    :param uploaded
+        :type bool
+    :param rbh_df
+        :type pd.core.frame.DataFrame
+    :param db_df
+        :type pd.core.frame.DataFrame
+    :param taxonomic_unit
+        :type str
+
+    :returns tax_counts
+        :type list
+
+'''
+def extract_taxonomic_information(logfile: str, uploaded: bool, rbh_df: pd.core.frame.DataFrame,
+                                  db_df: pd.core.frame.DataFrame, taxonomic_unit: str) -> list:
+    with open(logfile, 'w') as log:
+        try:
+            tax_counts = []
+            db_df = db_df.drop_duplicates(subset=["assembly_accession"], keep='first')
+
+            if uploaded is False:
+                log.write("INFO:working on downloaded refseq database entries\n")
+                acc = lambda ids: ids.split('_')[2] + '_' + ids.split('_')[3]
+                rbh_df['assembly_accession'] = rbh_df['sseqid']
+                rbh_df['assembly_accession'] = rbh_df['assembly_accession'].map(acc)
+                for query in rbh_df['qseqid'].unique():
+                    log.write("\tINFO:extracting taxonomic information for {}\n".format(query))
+                    df = rbh_df[rbh_df['qseqid'] == query]
+                    if len(df) > 0:
+                        df = df.drop_duplicates(subset=['assembly_accession'], keep='first')
+                        m_df = db_df.merge(df, on='assembly_accession')
+                        m_df = m_df.drop_duplicates(subset=['scomnames'], keep='first')
+                        if taxonomic_unit + '_x' in m_df.columns:
+                            values = m_df[taxonomic_unit + '_x'].value_counts()
+                        else:
+                            values = m_df[taxonomic_unit].value_counts()
+                        values = values.rename(query)
+                        tax_counts.append(values)
+
+            # database is based on custom uploaded files --> assembly accessions are not accurate, therfore filtering is based on taxids and scomnames
+            else:
+                log.write("INFO:working on uploaded database entries\n")
+                rbh_df.rename(columns={'staxids': 'species_taxid'}, inplace=True)
+                db_df = db_df.drop_duplicates(subset=["species_taxid"], keep='first')
+
+                for query in rbh_df['qseqid'].unique():
+                    log.write("\tINFO:extracting taxonomic information for {}\n".format(query))
+                    df = rbh_df[rbh_df['qseqid'] == query]
+                    if len(df) > 0:
+                        m_df = db_df.merge(df, on='species_taxid')
+                        m_df = m_df.drop_duplicates(subset=['scomnames'], keep='first')
+                        if taxonomic_unit + '_x' in m_df.columns:
+                            values = m_df[taxonomic_unit + '_x'].value_counts()
+                        else:
+                            values = m_df[taxonomic_unit].value_counts()
+                        values = values.rename(query)
+                        tax_counts.append(values)
+            log.write("DONE\n")
+            return tax_counts
+        except Exception as e:
+            log.write("ERROR:{}\n".format(e))
+            raise Exception("[-] ERROR in extract_taxonomic_information with exception: {}".format(e))
+
+'''tax_counts_to_db_statistic_tables
+
+    This function produces pandas dataframes with statistical information of the reciprocal best hits 
+    and the specified taxonomic unit. It writes two CSV files in the relevant project directory.
+    The first file inherits all hits for the specified taxonomic unit and the query sequences, the second
+    dataframe inherits normalized values of the first table. Normalization is based on the number of 
+    entries with the specified taxonomic unit within the database table.
+    
+    :param logfile
+        :type str
+    :param project_id
+        :type int
+    :param db_df
+        :type pd.core.frame.DataFrame
+    :param tax_counts
+        :type list
+    :param taxonomic_unit
+        :type str
+    
+    :returns (df, normalized_df)
+        :type (pd.core.frame.DataFrame, pd.core.frame.DataFrame)
+
+'''
+def tax_counts_to_db_statistic_tables(logfile: str, project_id: int, db_df: pd.core.frame.DataFrame,
+                                      tax_counts: list, taxonomic_unit: str) -> tuple:
+    with open(logfile, 'w') as log:
+        try:
+            log.write("INFO:starting statistical inference for {}\n".format(taxonomic_unit))
+            if len(tax_counts) > 0:
+
+                percentage = db_df['class'].value_counts()
+                percentage = percentage.rename('Database')
+                tax_counts.append(percentage)
+
+                log.write("INFO:trying to write table with {} values for each query\n".format(taxonomic_unit))
+                df = pd.DataFrame(tax_counts)
+                df = df.fillna(0)
+                df = df.transpose()
+                df_path = 'media/blast_projects/' + str(project_id) + '/database_statistics.csv'
+                df.to_csv(df_path)
+                log.write("INFO:produced {} table\n".format(df_path))
+
+                log.write("INFO:trying to normalize values\n")
+                f = lambda x, y: (100 / y) * x
+                normalizing_results = tax_counts
+                normalizing_results.append(percentage)
+                normalized_df = pd.DataFrame(normalizing_results)
+                normalized_df = normalized_df.transpose().fillna(0)
+                normalized_table = []
+
+                for query in normalized_df.columns:
+                    if query != 'Database':
+                        temp_res = f(normalized_df[query], percentage)
+                        temp_res.name = query
+                        normalized_table.append(temp_res)
+
+                log.write(
+                    "INFO:trying to write normalization table with percentage of {} values for each query\n".format(
+                        taxonomic_unit))
+                normalized_df = pd.DataFrame(normalized_table)
+                normalized_df = normalized_df.fillna(0)
+                df_path = 'media/blast_projects/' + str(project_id) + '/database_statistics_normalized.csv'
+                normalized_df.to_csv(df_path)
+                log.write("INFO:produced {} table\n".format(df_path))
+            else:
+                log.write("INFO:Dataframes are empty\n")
+                df = pd.DataFrame()
+                normalized_df = pd.DataFrame()
+            return df, normalized_df
+        except Exception as e:
+            log.write("ERROR:{}".format(e))
+            raise Exception("[-] ERROR in tax_counts_to_db_statistics_tables function with exception {}".format(e))
+
 #TODO extend this function to produce not only class based statistics
-#TODO refactor function, this function if too long and has several logic layers
 '''calculate_database_statistics
 
     This function calculates database statistics based on the reciprocal results of a finished pipeline project.
@@ -147,8 +291,7 @@ def calculate_database_statistics(project_id: int,logfile:str,user_email:str)->p
 
             log.write("INFO:Starting to calculate database statistics\n")
 
-            path_to_project='media/blast_projects/' + str(project_id)
-
+            path_to_project = 'media/blast_projects/' + str(project_id)
             project = py_db_service.get_project_by_id(project_id)
             forward_db = project.project_forward_database
             path_to_db_csv = forward_db.path_to_database_file
@@ -168,99 +311,26 @@ def calculate_database_statistics(project_id: int,logfile:str,user_email:str)->p
                 db_df = pd.read_csv(new_database_name, index_col=0)
                 log.write("INFO:Done loading result and database dataframe\n")
             else:
-                db_df=pd.read_csv(path_to_db_csv,index_col=0)
+                db_df = pd.read_csv(path_to_db_csv,index_col=0)
                 log.write("INFO:Done loading result and database dataframe\n")
                 log.write("INFO:Trying to add taxonomic information to dataframe ...\n")
                 add_tax_logfile=path_to_project+'/log/add_taxonomic_information_to_db.log'
-                tax_df=add_taxonomic_information_to_db(user_email, add_tax_logfile, list(db_df['taxid'].unique()))
+                tax_df = add_taxonomic_information_to_db(user_email, add_tax_logfile, list(db_df['taxid'].unique()))
                 tax_df['taxid'] = tax_df['taxid'].astype('int64')
                 db_df = db_df.merge(tax_df, on='taxid')
                 log.write("INFO:DONE with adding taxonomic information\n")
-
                 log.write("INFO:trying to write database csv file into project directory\n")
-
                 db_df.to_csv(new_database_name)
                 log.write("INFO:DONE writing database csv file\n")
 
-            #database is based on refseq genome files --> filtering based on assembly_accessions and scomnames
-            if forward_db.uploaded_files is False:
+            log.write("INFO:starting function extract_taxonomic_information ...")
+            logfile_tax_count_function=path_to_project+'log/extract_taxonomic_information.log'
+            tax_counts=extract_taxonomic_information(logfile_tax_count_function, forward_db.uploaded_files, result_data, db_df, 'class')
+            log.write("INFO:Done extracting list of taxonomic informations\n")
 
-                log.write("INFO:working on downloaded refseq database entries\n")
-
-                acc = lambda ids: ids.split('_')[2] + '_' + ids.split('_')[3]
-                result_data['assembly_accession'] = result_data['sseqid']
-
-                result_data['assembly_accession'] = result_data['assembly_accession'].map(acc)
-                class_counts = []
-                for query in result_data['qseqid'].unique():
-                    df = result_data[result_data['qseqid'] == query]
-                    if len(df) > 0:
-                        df = df.drop_duplicates(subset=['assembly_accession'], keep='first')
-                        m_df = db_df.merge(df, on='assembly_accession')
-                        m_df = m_df.drop_duplicates(subset=['scomnames'], keep='first')
-                        if 'class_x' in m_df.columns:
-                            values = m_df['class_x'].value_counts()
-                        else:
-                            values = m_df['class'].value_counts()
-                        values = values.rename(query)
-                        class_counts.append(values)
-
-            #database is based on custom uploaded files --> assembly accessions are not accurate, therfore filtering is based on taxids and scomnames
-            else:
-                log.write("INFO:working on uploaded database entries\n")
-                class_counts = []
-                result_data.rename(columns={'staxids': 'species_taxid'}, inplace=True)
-
-                for query in result_data['qseqid'].unique():
-                    df = result_data[result_data['qseqid'] == query]
-                    if len(df) > 0:
-                        db_df = db_df.drop_duplicates(subset=["assembly_accession"], keep='first')
-                        db_df = db_df.drop_duplicates(subset=["species_taxid"], keep='first')
-                        m_df = db_df.merge(df, on='species_taxid')
-                        m_df = m_df.drop_duplicates(subset=['scomnames'], keep='first')
-                        if 'class_x' in m_df.columns:
-                            values = m_df['class_x'].value_counts()
-                        else:
-                            values = m_df['class'].value_counts()
-                        values = values.rename(query)
-                        class_counts.append(values)
-
-            log.write("INFO:Done merging database and result dataframes\n")
             log.write("INFO:Starting to produce output tables and graphs\n")
-            if len(class_counts) > 0:
-                df = pd.DataFrame(class_counts)
-                df = df.fillna(0)
-                df = df.transpose()
-                df_path = 'media/blast_projects/' + str(project_id)+ '/database_statistics.csv'
-                df.to_csv(df_path)
-                for column in df.columns:
-                    df[column].plot.bar(figsize=(12, 8), title=column, grid=True)#TODO directly saving?
-                    plot_path = 'media/blast_projects/' + str(project_id)+ '/' + str(column) + '/' + 'database_result_statistics.png'
-                    plt.tight_layout()
-                    plt.savefig(plot_path)
-
-                log.write("INFO:trying to normalize values\n")
-                percentage = db_df['class'].value_counts()
-                percentage = percentage.rename('Normalizer')
-                f = lambda x, y: (100 / y) * x
-                normalizing_results = class_counts
-                normalizing_results.append(percentage)
-                normalized_df = pd.DataFrame(normalizing_results)
-                normalized_df = normalized_df.transpose().fillna(0)
-                normalized_table = []
-                for query in normalized_df.columns:
-                    if query != 'Normalizer':
-                        temp_res = f(normalized_df[query], normalized_df['Normalizer'])
-                        temp_res.name = query
-                        normalized_table.append(temp_res)
-
-                normalized_df = pd.DataFrame(normalized_table)
-                normalized_df = normalized_df.fillna(0)
-                df_path = 'media/blast_projects/' + str(project_id) + '/database_statistics_normalized.csv'
-                normalized_df.to_csv(df_path)
-            else:
-                log.write("INFO:Dataframe is empty\n")
-                df=pd.DataFrame()
+            logfile_tax_to_db_stat_function = path_to_project + 'log/tax_counts_to_db_statistics_tables.log'
+            df, normalized_df = tax_counts_to_db_statistic_tables(logfile_tax_to_db_stat_function, project_id, db_df, tax_counts, 'class')
             log.write("DONE\n")
             return df
 
@@ -336,4 +406,26 @@ def delete_database_statistics_task_and_output(project_id:int,logfile:str)->int:
     except Exception as e:
         log.write("ERROR: couldnt delete database statistics task with exception: {}\n".format(e))
         raise Exception("ERROR during removing database statistics output with exception: {}".format(e))
+
+
+
+
+'''transform_normalized_database_table_to_json
+
+'''
+def transform_normalized_database_table_to_json(project_id:int):
+    try:
+
+        df_path = 'media/blast_projects/' + str(project_id) + '/database_statistics_normalized.csv'
+        if isfile(df_path):
+            table = pd.read_csv(df_path,header=0,index_col=0)
+            json_records = table.reset_index().to_json(orient='records')
+            data = json.loads(json_records)
+            return data
+        else:
+            raise FileNotFoundError
+    except FileNotFoundError:
+        raise Exception("[-] File {} does not exist!".format('media/blast_projects/' + str(df_path)))
+    except Exception as e:
+        raise Exception("[-] Couldnt transform normalized database statistics to json with exception: {}".format(e))
 
