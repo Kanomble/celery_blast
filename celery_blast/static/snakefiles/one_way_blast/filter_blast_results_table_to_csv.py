0a4e1f361fe836500595b99e194bfab1416eb6fa
Kanomble <lujebecker@googlemail.com>
2024-03-24 19:43:48 +0100

    refactored handling of "no result" for specific query sequences in local One-Way-BLAST projects

--- celery_blast/static/snakefiles/one_way_blast/filter_blast_results_table_to_csv.py
+++ celery_blast/static/snakefiles/one_way_blast/filter_blast_results_table_to_csv.py
@@ -14,20 +14,27 @@ from os import mkdir
 RETURNCODE=5
 with open(snakemake.log['log'], 'w') as log_f:
     try:
+        log_f.write("INFO:starting to filter blast results ...\n")
         result_data=pd.read_table(snakemake.input['results_table'], header=0, index_col=0)
         result_data['qseqid'] = result_data['qseqid'].map(lambda line: line.split('.')[0])
         result_data['sacc_transformed'] = result_data['sacc'].map(lambda line: line.split('.')[0])
 
         no_hits = False
+        log_f.write("INFO:checking hits ...\n")
+
         if len(result_data["qseqid"]) == 1:
             if "no information" in list(result_data["qseqid"]):
                 no_hits = True
+        if no_hits == False:
+            log_f.write("INFO:found {} hits in the result table.\n".format(len(result_data["qseqid"])))
+        else:
+            log_f.write("INFO:there are no hits for you query proteins.\n")
 
         queries = {}
         queryfile = open(snakemake.input['query_file'], "r")
         for line in queryfile.readlines():
             if ">" in line:
-                prot_id = line.split(">")[1].split(' ')[0]
+                prot_id = line.split(">")[1].split(' ')[0].split(".")[0]
                 line = ' '.join(line.split(">")[1].split(' ')[1:]).rstrip()
                 queries[prot_id] = line
         queryfile.close()
@@ -35,6 +42,8 @@ with open(snakemake.log['log'], 'w') as log_f:
         qseqids = queries.keys()
         for qseq in qseqids:
             qseq_df = result_data[result_data["qseqid"]==qseq]
+            log_f.write("INFO:working with query: {}\n".format(qseq))
+            log_f.write("\tINFO:found {} hits for query sequence\n".format(len(qseq_df)))
             qseq_df = qseq_df.drop_duplicates(subset='sacc', keep='first')
             if len(qseq_df) > 50:
                 best_hits = math.ceil(len(qseq_df)*0.1)
@@ -43,17 +52,19 @@ with open(snakemake.log['log'], 'w') as log_f:
                 best_hits = math.ceil(len(qseq_df)*0.50)
                 qseq_df = qseq_df.nlargest(best_hits, "bitscore")
 
+            log_f.write("\tINFO:checking if directory for {} exists ...\n".format(qseq))
             if isdir(qseq) == False:
                 mkdir(qseq)
 
+            output_path = qseq + "/blast_results_with_tax.table"
+            log_f.write("\tINFO:writing output csv: {} to disc ...\n".format(output_path))
             qseq_df = qseq_df.reset_index(drop=True)
-
-            output_file_path = str(qseq) + '/' + 'blast_results_with_tax.table'
-            qseq_df.to_csv(output_file_path,sep='\t', header=list(qseq_df.columns))
+            qseq_df.to_csv(output_path,sep='\t', header=list(qseq_df.columns))
 
             try:
-                sacc_list= list(qseq_df['sseqid'].unique())
                 id_path= str(qseq) + '/' + 'target_sequence_ids.txt'
+                log_f.write("\tINFO:writing output id list: {} to disc ...\n".format(id_path))
+                sacc_list= list(qseq_df['sseqid'].unique())
                 output= open(id_path,'w')
                 for sacc in sacc_list:
                     output.write(sacc+'\n')
@@ -62,12 +73,12 @@ with open(snakemake.log['log'], 'w') as log_f:
             except Exception as e:
 
                 log_f.write('ERROR:' + "[-] Error during writing hit info as txt {} with Exception: {}".format(
-                    snakemake.output['ids'], e))
+                    id_path, e))
                 sys.exit(RETURNCODE)
 
-
-            log_f.write('DONE: The results table has been filtered for the best hits, which where written to {}\n'.format(
-                snakemake.output['ids']))
+            log_f.write("\tINFO:DONE\n")
+            log_f.write('DONE: The result table has been filtered for the best hits, which where written to {}\n'.format(
+                id_path))
 
     except Exception as e:
 
